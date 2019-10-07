a6894b2785ab56e1db1531c6a6a9250ca41c59a1
PabloTrianda <pablotrianda@gmail.com>
2019-10-07 17:09:29 -0300

    Update install_config.sh

--- install_config.sh
+++ install_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 echo --------------------------------------------------------------
-echo "Getting .vimrc file"               
+echo "Getting tmux.conf file"               
 echo --------------------------------------------------------------
 curl -fsSL https://raw.githubusercontent.com/pablotrianda/tmux-config/master/tmux.conf >> tmux.conf
 while true; do
