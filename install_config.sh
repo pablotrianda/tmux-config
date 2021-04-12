495c9bfc17632bd47d188b9998ef13ca3005f0be
PabloTrianda <pablotrianda@gmail.com>
2021-04-12 09:54:25 -0300

    Update install_config.sh

--- install_config.sh
+++ install_config.sh
@@ -15,15 +15,5 @@ esac
 done
 echo "Replacing tmux file"
 cp tmux.conf ~/.tmux.conf
-while true; do
-		echo "Add tmux to zsh?"
-		read -p "Continue? [y/n]: " yn
-		case $yn in
-				y) break;;
-				n) exit;;
-				*) ;;
-		esac
-done
-echo "case $- in *i*) if [ -z "$TMUX"]; then exec tmux -f ~/.zshrc; fi;;esac" >> ~/.zshrc
 echo "Tmux is OK!"
 
